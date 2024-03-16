9dc213b3d6e72b5faafe72089ef4b41cf08d23b4
Timan Noel <tnoel7@msn.com>
2024-03-16 19:02:52 +0000

    Fixing sass build issues

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { block } from 'bem-cn';
 import './navigation.scss';
 
 const useStyles = makeStyles(theme => ({
@@ -11,6 +12,9 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
   },
 }));
+
+const CSS_BLOCK_NAME = 'navigation';
+const blk = block(CSS_BLOCK_NAME);
 
 function Navigation() {
   const classes = useStyles();
@@ -21,12 +25,13 @@ function Navigation() {
   }
 
   return (
-    <div className={classes.root}>
+    <div className={blk()}>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} centered>
           <Tab label="Films" />
           <Tab label="TV Shows" />
           <Tab label="Books" />
+          <Tab label="Restaurants" />
         </Tabs>
       </AppBar>
     </div>
