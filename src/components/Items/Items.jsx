4cdcd3b9db0e3e2b5c9625d7b70bd277f309825e
Timan Noel <tnoel7@msn.com>
2024-03-18 21:42:48 +0000

    Making mobile compatible. Small tweaks.

---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { block } from 'bem-cn';
+import { Item } from "../Item"
+import PropTypes from "prop-types";
+import './items.scss';
+
+const CSS_BLOCK_NAME = 'items';
+const blk = block(CSS_BLOCK_NAME);
+
+function Items({ currentData }) {
+
+  return (
+    <div className={blk()}>
+      {currentData.map(([name, rating, review]) => (
+        <Item 
+          key={name}
+          name={name}
+          rating={rating}
+          review={review}
+        />
+      ))}
+    </div>
+  );
+}
+
+Items.propTypes = {
+  currentData: PropTypes.array.isRequired,
+};
+
+export default Items;
