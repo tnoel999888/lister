b862704fb18037d9bcfcb24c5abed0514536e3b6
Timan Noel <tnoel7@msn.com>
2024-03-21 22:22:49 +0000

    [18] Removing unused imports

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { block } from 'bem-cn';
 import { Item } from "../Item"
 import PropTypes from "prop-types";
-import { getRatingInfo } from '../consts';
 
 import './items.scss';
 
