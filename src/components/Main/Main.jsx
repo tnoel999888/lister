edbbc577bc43139b313967e1ae643c4dcb17bd0b
Timan Noel <tnoel7@msn.com>
2024-09-17 21:05:58 +0100

    [47] Fixing search bug

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Items } from "../Items";
+import { Controls } from "../Controls";
+import { TopTen } from "../TopTen";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { block } from 'bem-cn';
+import { getRatingInfo } from '../consts';
+import Papa from "papaparse";
+
+import './main.scss';
+
+const CSS_BLOCK_NAME = 'main';
+const blk = block(CSS_BLOCK_NAME);
+
+function Main({ ratingsFile }) {
+
+  const [currentData, setCurrentData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  let dataLoaded = false;
+
+  if (originalData.length) {
+    dataLoaded = true;
+  }
+
+  useEffect(() => {
+    Papa.parse(
+      ratingsFile, 
+      {
+        skipEmptyLines: true,
+        download: true,
+        complete: (results) => {
+          results.data.shift(); // remove header line
+          const reversedData = [...results.data].reverse(); // reverse chronological order
+          reversedData.map((data, index) => {
+            const rating = data[1];
+            const ratingInfo = getRatingInfo(rating);
+            data.push(ratingInfo.rank);
+            return data.push(index);
+          });
+          setOriginalData(reversedData);
+          setCurrentData(reversedData);
+        },
+        error: (error, file) => {
+          console.log('Error while parsing:', error, file);
+        },
+      }
+    );
+  }, [ratingsFile]);
+
+  return (
+    <div className={blk()}>
+      { dataLoaded ?
+        <div className={blk("content")}>
+          <TopTen originalData={originalData} />
+          <Controls 
+            originalData={originalData}
+            currentData={currentData}
+            setCurrentData={setCurrentData}
+          />
+          <Items currentData={currentData} />
+        </div>
+        : 
+        <div className={blk("loading-spinner")}>
+          <CircularProgress size={80} style={{ margin: "auto", color: "#1F96F3" }}/>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default Main;
